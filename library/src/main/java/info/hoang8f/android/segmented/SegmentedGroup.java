c7feffd8cf7f3c7e22e1df1277cf2c0561586a49
Le Van Hoang <hoang8f@gmail.com>
2015-04-07 02:28:30 +0900

    code refactoring

--- library/src/main/java/info/hoang8f/android/segmented/SegmentedGroup.java
+++ library/src/main/java/info/hoang8f/android/segmented/SegmentedGroup.java
@@ -35,16 +35,18 @@ public class SegmentedGroup extends RadioGroup {
     }
 
     /* Reads the attributes from the layout */
-    private void initattrs(AttributeSet attrs){
+    private void initAttrs(AttributeSet attrs) {
         TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.SegmentedGroup,
                 0, 0);
         try {
-            mMarginDp = (int) typedArray.getDimension(R.styleable.SegmentedGroup_border_width
-                    , getResources().getDimension(R.dimen.radio_button_stroke_border));
-            mCornerRadius = typedArray.getDimension(R.styleable.SegmentedGroup_corner_radius
-                    , getResources().getDimension(R.dimen.radio_button_conner_radius));
+            mMarginDp = (int) typedArray.getDimension(
+                    R.styleable.SegmentedGroup_border_width,
+                    getResources().getDimension(R.dimen.radio_button_stroke_border));
+            mCornerRadius = typedArray.getDimension(
+                    R.styleable.SegmentedGroup_corner_radius,
+                    getResources().getDimension(R.dimen.radio_button_conner_radius));
         } finally {
             typedArray.recycle();
         }
@@ -56,7 +58,7 @@ public class SegmentedGroup extends RadioGroup {
         mTintColor = resources.getColor(R.color.radio_button_selected_color);
         mMarginDp = (int) getResources().getDimension(R.dimen.radio_button_stroke_border);
         mCornerRadius = getResources().getDimension(R.dimen.radio_button_conner_radius);
-        initattrs(attrs);
+        initAttrs(attrs);
         mLayoutSelector = new LayoutSelector(mCornerRadius);
     }
 
@@ -85,8 +87,7 @@ public class SegmentedGroup extends RadioGroup {
             updateBackground(child);
 
             // If this is the last view, don't set LayoutParams
-            if (i == count - 1)
-                break;
+            if (i == count - 1) break;
 
             LayoutParams initParams = (LayoutParams) child.getLayoutParams();
             LayoutParams params = new LayoutParams(initParams.width, initParams.height, initParams.weight);
@@ -134,7 +135,6 @@ public class SegmentedGroup extends RadioGroup {
         }
     }
 
-
     /*
      * This class is used to provide the proper layout based on the view.
      * Also provides the proper radius for corners.
@@ -142,7 +142,7 @@ public class SegmentedGroup extends RadioGroup {
      * float tables for setting the radius via Gradient.setCornerRadii are used instead
      * of multiple xml drawables.
      */
-    private class LayoutSelector{
+    private class LayoutSelector {
 
         private int children;
         private int child;
@@ -152,35 +152,35 @@ public class SegmentedGroup extends RadioGroup {
         private float r;    //this is the radios read by attributes or xml dimens
         private final float r1 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP
                 , 0.1f, getResources().getDisplayMetrics());    //0.1 dp to px
-        private final float[] rleft;    // left radiobutton
-        private final float[] rright;   //right radiobutton
-        private final float[] rmiddle;  //middle radiobutton
-        private final float[] rdefault; //default radiobutton
-        private final float[] rtop;     //top radiobutton
-        private final float[] rbot;     //bot radiobutton
-        private float[] radii;          //result radii float table
+        private final float[] rLeft;    // left radio button
+        private final float[] rRight;   // right radio button
+        private final float[] rMiddle;  // middle radio button
+        private final float[] rDefault; // default radio button
+        private final float[] rTop;     // top radio button
+        private final float[] rBot;     // bot radio button
+        private float[] radii;          // result radii float table
 
-        public LayoutSelector(float cornerRadius){
+        public LayoutSelector(float cornerRadius) {
             children = -1; // Init this to force setChildRadii() to enter for the first time.
             child = -1; // Init this to force setChildRadii() to enter for the first time
             r = cornerRadius;
-            rleft = new float[]{r, r, r1, r1, r1, r1, r, r};
-            rright = new float[]{r1, r1, r, r, r, r, r1, r1};
-            rmiddle = new float[]{r1, r1, r1, r1, r1, r1, r1, r1};
-            rdefault = new float[]{r, r, r, r, r, r, r, r};
-            rtop = new float[]{r, r, r, r, r1, r1, r1, r1};
-            rbot = new float[]{r1, r1, r1, r1, r, r, r, r};
+            rLeft = new float[]{r, r, r1, r1, r1, r1, r, r};
+            rRight = new float[]{r1, r1, r, r, r, r, r1, r1};
+            rMiddle = new float[]{r1, r1, r1, r1, r1, r1, r1, r1};
+            rDefault = new float[]{r, r, r, r, r, r, r, r};
+            rTop = new float[]{r, r, r, r, r1, r1, r1, r1};
+            rBot = new float[]{r1, r1, r1, r1, r, r, r, r};
         }
 
-        private int getChildren(){
-             return SegmentedGroup.this.getChildCount();
+        private int getChildren() {
+            return SegmentedGroup.this.getChildCount();
         }
 
-        private int getChildIndex(View view){
+        private int getChildIndex(View view) {
             return SegmentedGroup.this.indexOfChild(view);
         }
 
-        private void setChildRadii(int newChildren, int newChild){
+        private void setChildRadii(int newChildren, int newChild) {
 
             // If same values are passed, just return. No need to update anything
             if (children == newChildren && child == newChild)
@@ -190,35 +190,30 @@ public class SegmentedGroup extends RadioGroup {
             children = newChildren;
             child = newChild;
 
-            // if there is only one child provide the default radiobutton
-            if (children == 1)
-                radii = rdefault;
-            else if (child == 0) { //left or top
-                if (getOrientation() == LinearLayout.HORIZONTAL)   //left
-                    radii = rleft;
-                else    //top
-                    radii = rtop;
-            } else if (child == children - 1){  //right or bottom
-                if (getOrientation() == LinearLayout.HORIZONTAL)   //right
-                    radii = rright;
-                else    //bottom
-                    radii = rbot;
-            } else     //middle
-                radii = rmiddle;
+            // if there is only one child provide the default radio button
+            if (children == 1) {
+                radii = rDefault;
+            } else if (child == 0) { //left or top
+                radii = (getOrientation() == LinearLayout.HORIZONTAL) ? rLeft : rTop;
+            } else if (child == children - 1) {  //right or bottom
+                radii = (getOrientation() == LinearLayout.HORIZONTAL) ? rRight : rBot;
+            } else {  //middle
+                radii = rMiddle;
+            }
         }
 
         /* Returns the selected layout id based on view */
-        public int getSelected(){
+        public int getSelected() {
             return SELECTED_LAYOUT;
         }
 
         /* Returns the unselected layout id based on view */
-        public int getUnselected(){
-             return UNSELECTED_LAYOUT;
+        public int getUnselected() {
+            return UNSELECTED_LAYOUT;
         }
 
         /* Returns the radii float table based on view for Gradient.setRadii()*/
-        public float[] getChildRadii(View view){
+        public float[] getChildRadii(View view) {
             int newChildren = getChildren();
             int newChild = getChildIndex(view);
             setChildRadii(newChildren, newChild);
