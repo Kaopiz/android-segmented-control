f58813c380211fdf457fbb1a04ecc60909ebfad9
Evgeny Razumov <razumov.evgeni@gmail.com>
2018-05-07 01:26:37 +0300

    unckecked text color attr

--- library/src/main/java/info/hoang8f/android/segmented/SegmentedGroup.java
+++ library/src/main/java/info/hoang8f/android/segmented/SegmentedGroup.java
@@ -29,6 +29,7 @@ public class SegmentedGroup extends RadioGroup {
     private int mTintColor;
     private int mUnCheckedTintColor;
     private int mCheckedTextColor = Color.WHITE;
+    private int mUnCheckedTextColor;
     private LayoutSelector mLayoutSelector;
     private Float mCornerRadius;
     private OnCheckedChangeListener mCheckedChangeListener;
@@ -40,6 +41,7 @@ public class SegmentedGroup extends RadioGroup {
         resources = getResources();
         mTintColor = resources.getColor(R.color.radio_button_selected_color);
         mUnCheckedTintColor = resources.getColor(R.color.radio_button_unselected_color);
+        mUnCheckedTextColor = resources.getColor(R.color.radio_button_unselected_text_color);
         mMarginDp = (int) getResources().getDimension(R.dimen.radio_button_stroke_border);
         mCornerRadius = getResources().getDimension(R.dimen.radio_button_conner_radius);
         mLayoutSelector = new LayoutSelector(mCornerRadius);
@@ -50,6 +52,7 @@ public class SegmentedGroup extends RadioGroup {
         resources = getResources();
         mTintColor = resources.getColor(R.color.radio_button_selected_color);
         mUnCheckedTintColor = resources.getColor(R.color.radio_button_unselected_color);
+        mUnCheckedTextColor = resources.getColor(R.color.radio_button_unselected_text_color);
         mMarginDp = (int) getResources().getDimension(R.dimen.radio_button_stroke_border);
         mCornerRadius = getResources().getDimension(R.dimen.radio_button_conner_radius);
         initAttrs(attrs);
@@ -83,6 +86,10 @@ public class SegmentedGroup extends RadioGroup {
             mUnCheckedTintColor = typedArray.getColor(
                     R.styleable.SegmentedGroup_sc_unchecked_tint_color,
                     getResources().getColor(R.color.radio_button_unselected_color));
+
+            mUnCheckedTextColor = typedArray.getColor(
+                    R.styleable.SegmentedGroup_sc_unchecked_text_color,
+                    getResources().getColor(R.color.radio_button_unselected_text_color));
         } finally {
             typedArray.recycle();
         }
@@ -108,6 +115,11 @@ public class SegmentedGroup extends RadioGroup {
 
     public void setUnCheckedTintColor(int unCheckedTintColor, int unCheckedTextColor) {
         mUnCheckedTintColor = unCheckedTintColor;
+        updateBackground();
+    }
+
+    public void setUnCheckedTextColor(int unCheckedTextColor) {
+        mUnCheckedTextColor = unCheckedTextColor;
         updateBackground();
     }
 
@@ -140,7 +152,7 @@ public class SegmentedGroup extends RadioGroup {
         ColorStateList colorStateList = new ColorStateList(new int[][]{
                 {-android.R.attr.state_checked},
                 {android.R.attr.state_checked}},
-                new int[]{mTintColor, mCheckedTextColor});
+                new int[]{mUnCheckedTextColor, mCheckedTextColor});
         ((Button) view).setTextColor(colorStateList);
 
         //Redraw with tint color
